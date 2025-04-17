fb105badb365efb8d77ed66fcad876ce6d206598
Bruno Arend <bruno.arend300@gmail.com>
2025-04-16 21:34:22 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exemplo1;
+
+Importacao importacao = new();
+
+var produtos = importacao.ConverterParaLista();
+
+foreach (var produto in produtos)
+{
+    Console.WriteLine(produto.Id.ToString() + " " + produto.Nome);
+}
+
+
+Console.WriteLine("Hello, World!");
+
